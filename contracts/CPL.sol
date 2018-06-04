885866266519de2e09c3fee5e0da10003508fc81
Frank Tsai <franks0059@gmail.com>
2018-06-05 03:53:03 +0800

    rewrite contract

--- contracts/CPL.sol
+++ contracts/CPL.sol
@@ -1,14 +1,8 @@
 pragma solidity ^0.4.20;
 //import "browser/CPT.sol";
-import "./CPT.sol"
+import "./CPT.sol";
 contract CPL {
    
-  /* *** General *** */
-  address public contractOwner;       // The one setting up the contract
-  //address public freeLancer; // The address which will get the funds.
-  //TokenERC20 public token;  
-  uint256 public tokensForSale;
-  
   /* *** Modifiers *** */
   //modifier onlyOwner{ require(contractOwner == msg.sender); _; }
   
@@ -18,64 +12,47 @@ contract CPL {
   struct ContractBox {
     bytes32 name;
     bytes32 ipfs;
-	TokenERC20 price;
+	uint256 price;
 	address contractor;
+	address issuer;
 	uint state; // 0 : pendding  ; 1 : launch ;  2 : discontinued
-	uint datanumber;
   }
 
-  function setContractOwner(address _contractOwner) public {
-    require(contractOwner == address(0));
-    contractOwner = _contractOwner;
-  }
-  
-  //reset  contractOwner and freeLancer
-  function resetContractor()public{
-	  require(contractOwner != address(0));
-	  contractOwner = address(0);
-  }
-
-	function createProject(bytes32 _name , bytes32 _ipfs, TokenERC20 _priceProject) public {
-	    require(contractOwner != address(0));
-      Contracts[contractOwner].push(ContractBox({
+	function createProject(bytes32 _name, bytes32 _ipfs, uint256 _priceProject) public {
+      	Contracts[msg.sender].push(ContractBox({
           name: _name,
           ipfs: _ipfs,
           price: _priceProject,
           contractor: address(0),
-          state: 0,
-		  datanumber : 0
+		  issuer: msg.sender,
+          state: 0
       }));
 	}
 
-  event search(bytes32 names, bytes32 ipfss);
-  event search_free(bytes32 names, bytes32 ipfss);
-  
-  function searchContract(address _person) public {
-	
-     for(uint i = 0 ; i < Contracts[_person].length; i++){
-        emit search(Contracts[_person][i].name,Contracts[_person][i].ipfs);
-	   }
+	function getContractCount(address _personalAddr) public returns(uint length) {
+		return Contracts[_personalAddr].length;
 	}
-	
-	function searchContract_free(address _person) public {
+	function getContract(address _personalAddr, uint index) public returns (bytes32 ipfs) {
+		return Contracts[_personalAddr][index].ipfs;
+	}
+	function getContractFreeCount(address _personalAddr) public returns(uint length) {
+		return Contracts_free[_personalAddr].length;
+	}
+	function getContractFree(address _personalAddr, uint index) public returns(bytes32 ipfs) {
+		return Contracts_free[_personalAddr][index].ipfs;
+	}
 
-     for(uint i = 0 ; i < Contracts_free[_person].length; i++){
-       emit search_free(Contracts_free[_person][i].name,Contracts_free[_person][i].ipfs);
-	   }
 	
-	}
-	
-	function get_project_contractowner(bytes32 _ipfs) private returns (uint){
+	function getProjectContractOwner(address _owner, bytes32 _ipfs) private returns (uint) {
 	    uint i = 0 ;
-	    for(i = 0 ; i < Contracts[contractOwner].length ; i++)
-	    {
-	        if(Contracts[contractOwner][i].ipfs == _ipfs)
+	    for(i = 0 ; i < Contracts[_owner].length ; i++) {
+	        if(Contracts[_owner][i].ipfs == _ipfs)
 	            break;
 	    }
 	    return(i);
 	}
 	
-	function get_project_free(bytes32 _ipfs, address _freeLancer) private returns (uint){
+	function getProjectFree(address _freeLancer, bytes32 _ipfs) private returns (uint){
 	    uint i = 0 ;
 	    for(i = 0 ; i < Contracts_free[_freeLancer].length ; i++)
 	    {
@@ -86,60 +63,57 @@ contract CPL {
 	}
 	
 	// if you want to update contract, you should set toke first
-	function updateContract(bytes32 old_ipfs, bytes32 new_ipfs)public{
-	    require(contractOwner != address(0));
-	    uint i = get_project_contractowner(old_ipfs);
-		address _freeLancer = Contracts[contractOwner][i].contractor;
-	    require(Contracts[contractOwner][i].state == 1);//launch
-		uint j = Contracts[contractOwner][i].datanumber;
-		require(Contracts_free[_freeLancer][j].state == 1);
-		Contracts[contractOwner][i].ipfs = new_ipfs;
-		Contracts_free[_freeLancer][j].ipfs = new_ipfs;
+	function updateContractFromOwner(bytes32 old_ipfs, bytes32 new_ipfs)public{
+	    require(msg.sender != address(0));
+	    uint i = getProjectContractOwner(msg.sender , old_ipfs);
+		address _freeLancer = Contracts[msg.sender][i].contractor;
+	    require(Contracts[msg.sender][i].state == 1);//launch
+
+		uint j = getProjectFree(_freeLancer, old_ipfs);
 		
-	    
+		require(Contracts_free[_freeLancer][j].state == 1);
+		Contracts[msg.sender][i].ipfs = new_ipfs;
+		Contracts_free[_freeLancer][j].ipfs = new_ipfs;
+	}
+
+	function updateContractFromFreelancer(bytes32 old_ipfs, bytes32 new_ipfs)public{
+	    require(msg.sender != address(0));
+	    uint i = getProjectFree(msg.sender, old_ipfs);
+		address _issuer = Contracts_free[msg.sender][i].issuer;
+	    require(Contracts[msg.sender][i].state == 1);//launch
+
+		uint j = getProjectContractOwner(_issuer, old_ipfs);
+
+		require(Contracts[_issuer][j].state == 1);
+		Contracts[_issuer][i].ipfs = new_ipfs;
+		Contracts_free[msg.sender][j].ipfs = new_ipfs;
 	}
 	
-	function receiveContract(address _contractOwner, bytes32 _ipfs , address _freeLancer)public{
-	    //require(freeLancer == address(0));
-	    require(contractOwner == _contractOwner);
-	    uint i = get_project_contractowner(_ipfs);
+	function receiveContract(address _contractOwner, bytes32 _ipfs)public{
+	    require(msg.sender != address(0));
+	   
+	    uint i = getProjectContractOwner(_contractOwner, _ipfs);
 		//freeLancer = _freeLancer;
-	    require(Contracts[contractOwner][i].state == 0);//pendding
-	    require(Contracts[contractOwner][i].contractor == address(0));
-	    Contracts[contractOwner][i].state = 1;
-	    Contracts[contractOwner][i].contractor = _freeLancer ;
+	    require(Contracts[_contractOwner][i].state == 0);//pendding
+	    require(Contracts[_contractOwner][i].contractor == address(0));
+	    Contracts[_contractOwner][i].state = 1;
+	    Contracts[_contractOwner][i].contractor = msg.sender ;
 	    // freelancer information
-	    Contracts_free[_freeLancer].push(ContractBox({
-          name: Contracts[contractOwner][i].name,
-          ipfs: Contracts[contractOwner][i].ipfs,
-          price: Contracts[contractOwner][i].price,
-          contractor: contractOwner,
-          state: 1,
-		  datanumber : 0
-      }));
-	    uint j = get_project_free(_ipfs, _freeLancer);
-	    Contracts[contractOwner][i].datanumber = j ;
-	    
+	    Contracts_free[msg.sender].push(Contracts[_contractOwner][i]);
 	}
 
     
-	function terminateContract(address _contractOwner, address _freeLancer, bytes32 _ipfs  , uint256 percent) public payable{
-	    //require(freeLancer == _freeLancer);
-	    require(contractOwner == _contractOwner);
-	    uint i = get_project_contractowner(_ipfs);
-		require(_freeLancer == Contracts[contractOwner][i].contractor);
-	    require(Contracts[contractOwner][i].state == 1);//launch
-	    TokenERC20 token = Contracts[contractOwner][i].price;
-		tokensForSale = token.balanceOf(contractOwner);
-		token.approve(contractOwner , tokensForSale);
-	    token.transfer(_freeLancer, (tokensForSale * percent / 100));
-		if(percent != 100)
-		{
-			token.transfer(contractOwner, (tokensForSale * (100-percent) / 100));
-		}
-	    Contracts[contractOwner][i].state = 2; //terminate
-		uint j = Contracts[contractOwner][i].datanumber;
-		Contracts_free[_freeLancer][j].state = 2 ;
+	function terminateContract(address _contractOwner, address _freeLancer, bytes32 _ipfs  , uint256 percent, address tokenAddress) public payable{
+	    uint contractIdByContractOwner = getProjectContractOwner(_contractOwner, _ipfs);
+		require(_freeLancer == Contracts[_contractOwner][contractIdByContractOwner].contractor);
+	    require(Contracts[_contractOwner][contractIdByContractOwner].state == 1);//launch
+
+		CPT coinContract = CPT(tokenAddress);
+		uint256 price = Contracts[_contractOwner][contractIdByContractOwner].price;
+	    coinContract.transferFrom(_contractOwner, _freeLancer, (price * (percent / 100)));
+	    Contracts[_contractOwner][contractIdByContractOwner].state = 2; //terminate
+		uint contractIdByFreelancer = getProjectFree(_contractOwner, _ipfs);
+		Contracts_free[_freeLancer][contractIdByFreelancer].state = 2 ;
 	}
 }
 
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,8 @@
 var SimpleStorage = artifacts.require("./SimpleStorage.sol");
-
+var CPL = artifacts.require("./CPL.sol");
+var CPT = artifacts.require("./CPT.sol");
 module.exports = function(deployer) {
   deployer.deploy(SimpleStorage);
+  deployer.deploy(CPL);
+  deployer.deploy(CPT);
 };
